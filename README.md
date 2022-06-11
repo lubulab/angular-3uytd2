cd6103c3b12e3ae5798d8c53bc85c27d930d7912
lubulab <davide.vacatello@gmail.com>
2022-06-11 19:04:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-3uytd2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-3uytd2)
---
+++ src/app/mock.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MockService {
+  constructor(private http: HttpClient) {}
+
+  livenessInit(sessionId: string, nmoves: string): Observable<any> {
+    return of({
+      esito: 'OK',
+      errorCode: null,
+      errorDescription: null,
+      key: 'ADFTG453',
+      moves: ['Right', 'Center', 'Left', 'Center', 'Right', 'Center'],
+    });
+  }
+
+  faceMatch(deviceType: string, img: string, key: string, video: any) {
+    const data = {
+      deviceType: deviceType,
+      img: img,
+      key: key,
+      video: video,
+    };
+
+    return this.http.post('https://httpbin.org/post', data);
+/*
+    return of({
+      esito: 'OK',
+      errorCode: null,
+      errorDescription: null,
+      images: [
+        {
+          faceId: '2',
+          isIdentical: 'false',
+          confidence: 0.4860555501592239,
+        },
+        {
+          faceId: '3',
+          isIdentical: 'false',
+          confidence: 0.4980744067454498,
+        },
+      ],
+    });
+    */
+  }
+}
