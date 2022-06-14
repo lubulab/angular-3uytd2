6c93c036f1c524639974014bbb247e124e04f555
lubulab <davide.vacatello@gmail.com>
2022-06-14 09:03:15 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   ngOnInit(): void {
-    console.log('v1.1');
+    console.log('v1.2');
   }
 
 }
